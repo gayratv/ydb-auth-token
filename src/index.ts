4cad7d24f5887b3312e49a3c2e7ab73530c4ca38
Ulibka68 <gena68@inbox.ru>
2022-01-13 20:55:23 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { config } from 'dotenv';
+import { Driver, getLogger, TokenAuthService } from 'ydb-sdk';
+import fs from 'fs';
+import path from 'path';
+
+config({ path: 'env.local' });
+
+const databaseName = process.env.DATABASENAME!;
+const logger = getLogger({ level: process.env.LOGLEVEL! });
+const entryPoint = process.env.ENTRYPOINT!;
+const driver: Driver = null as unknown as Driver; // singleton
+
+export async function run() {
+    logger.debug('Driver initializing...');
+
+    const accessToken = fs.readFileSync(path.resolve(__dirname, 'iam-token.txt'), 'utf8').trim();
+
+    const authService = new TokenAuthService(accessToken, databaseName);
+    const driver = new Driver(entryPoint, databaseName, authService);
+    const timeout = 10000;
+    const drvInitResult = await driver.ready(timeout);
+    if (!drvInitResult) {
+        // if (!(await driver.ready(timeout))) {
+        logger.fatal(`Driver has not become ready in ${timeout}ms!`);
+        process.exit(1);
+    }
+}
+
+(async () => {
+    console.log('Инициализация драйвера');
+    await run();
+    console.log('Драйвер инициализирован - токен сработал');
+})();
